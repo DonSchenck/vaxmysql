75b570c4381c1947337cc52804dbd4eabd3ce440
Don Schenck <don.schenck@gmail.com>
2022-05-31 16:00:18 -0400

    fixes

--- create_table_vaccination_summaries.sql
+++ create_table_vaccination_summaries.sql
@@ -1,8 +1,8 @@
 USE vaxdb;
 GO
 CREATE TABLE vaccination_summaries (
-	location_code     char(  2 ),
-	reporting_date    char( 10 ),
+	location_code     char( 2 ),
+	reporting_date    char( 8 ),
 	vaccination_count int,
 	PRIMARY KEY ( location_code, reporting_date )
 );
